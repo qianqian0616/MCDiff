284af5d6e13a72155fc8d7c1a1d33f0f8773a064
shuishui616 <119089994+shuishui616@users.noreply.github.com>
2025-04-25 20:39:56 +0800

    Update seed.py

--- seed.py
+++ seed.py
@@ -1,1 +1,9 @@
+import random
+import numpy as np
+import torch
 
+def worker_seeds(rank=0, seed=42):
+    worker_seed = rank + seed
+    random.seed(worker_seed)
+    np.random.seed(worker_seed)
+    torch.manual_seed(worker_seed)
